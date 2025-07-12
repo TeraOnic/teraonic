0118829f4f19c11c6f1ab9b9d76ee356bf595c19
M-Taqi796 <itstaqi2919@gmail.com>
2025-07-12 20:57:11 +0500

    add: hover effect added to services card

--- src/components/services-card/ServicesCard.component.jsx
+++ src/components/services-card/ServicesCard.component.jsx
@@ -2,9 +2,9 @@ import WebDevImg from "../../assets/services/4.png";
 import Arrow from "../../assets/services/arrow.svg";
 export default function ServicesCard({ title, picture }) {
   return (
-    <div>
+    <div className="hover:shadow-[12px_22px_4px_rgba(0,0,0,0.25)] transition-all duration-300 rounded-[20px] group">
       <img src={picture} alt="Service Image" className="w-[394px] h-[394px] rounded-tl-[20px] rounded-tr-[20px] object-cover object-center" />
-      <div className="bg-[#2C2C2C] text-[#ffffff] w-[394px] px-[26px] py-[28px] flex justify-between rounded-bl-[20px] rounded-br-[20px] text-2xl">
+      <div className="bg-[#2C2C2C] text-[#ffffff] w-[394px] px-[26px] py-[28px] flex justify-between rounded-bl-[20px] rounded-br-[20px] text-2xl group-hover:bg-[#007DDC] transition-all duration-300">
         <p>{title}</p>
         <img src={Arrow} alt="Arrow icon" />
       </div>
--- src/components/services/Services.component.jsx
+++ src/components/services/Services.component.jsx
@@ -2,18 +2,19 @@ import ServicesCard from "../services-card/ServicesCard.component";
 import WebDevImg from "../../assets/services/4.png";
 import MobDevImg from "../../assets/services/5.png";
 import AiServImg from "../../assets/services/6.png";
+import QAImg from "../../assets/services/9.jpg";
 import UiUxImg from "../../assets/services/7.png";
 import GDImg from "../../assets/services/8.png";
 
 export default function Services() {
   return (
-    <div>
+    <div className="mx-[124px] mb-[70px]">
       <h1 className="text-[42px] text-center mb-[70px]">Our Services</h1>
-      <div className="flex mx-[124px] justify-between flex-wrap gap-y-12">
+      <div className="flex justify-between flex-wrap gap-y-12">
         <ServicesCard title="Web Development" picture={WebDevImg} />
         <ServicesCard title="Mobile App Development" picture={MobDevImg} />
         <ServicesCard title="AI Services" picture={AiServImg} />
-        <ServicesCard title="Quality Assurance" picture={UiUxImg} />
+        <ServicesCard title="Quality Assurance" picture={QAImg} />
         <ServicesCard title="UI/UX Design" picture={UiUxImg} />
         <ServicesCard title="Graphic Design" picture={GDImg} />
       </div>
