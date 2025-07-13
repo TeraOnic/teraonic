4a7313b3e74fbf85c1bd70464dad6f287434f3f9
M-Taqi796 <itstaqi2919@gmail.com>
2025-07-13 19:51:42 +0500

    add: navagation bar and primary buttom

--- src/components/navigation-bar/NavigationBar.component.jsx
+++ src/components/navigation-bar/NavigationBar.component.jsx
@@ -1,7 +1,18 @@
+import Logo from "../../assets/logo/Logo-TeraOnic.svg";
+import PrimaryBtn from "../primary-btn/PrimaryBtn.component";
 export default function NavigationBar() {
   return (
     <nav>
-      <div className="font-poppins">I am Navbar</div>
+      <div className="font-poppins mx-32 flex justify-between py-4">
+        <img src={Logo} alt="" />
+        <div className="flex gap-16 items-center text-sm mr-0.5">
+          <a href="">Home</a>
+          <a href="">Our Projects</a>
+          <a href="">About Us</a>
+          <a href="">Contact Us</a>
+          <PrimaryBtn text="Get Started" />
+        </div>
+      </div>
     </nav>
   );
 }
