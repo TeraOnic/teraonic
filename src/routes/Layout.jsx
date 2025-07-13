f3ca805bf571f370b7d1e2818e4c8fd51c1c7a68
asad-ullah-turab <asadullahturab47@gmail.com>
2025-07-13 14:59:29 +0500

    Fix: NavigationBar component spelling

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import NavigationBar from "../components/naivgation-bar/NavigationBar.component";
+import NavigationBar from "../components/navigation-bar/NavigationBar.component";
 import Footer from "../components/footer/Footer.component";
 
 export default function Layout() {
