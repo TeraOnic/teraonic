bb79b5ac3b29c92d97a2344509d2bf9bb5a326d2
M-Taqi796 <itstaqi2919@gmail.com>
2025-07-31 20:22:11 +0500

    updated: primary and secondry buttons

--- src/components/primary-btn/PrimaryBtn.component.jsx
+++ src/components/primary-btn/PrimaryBtn.component.jsx
@@ -1,6 +1,6 @@
 export default function PrimaryBtn({ text }) {
   return (
-    <button className="bg-secondary text-white px-3 py-2 rounded-sm text-sm font-poppins hover:bg-primary transition-all duration-300">
+    <button className="bg-secondary text-white p-4 rounded-[0.5rem] w-44 text-sm font-poppins hover:bg-primary transition-all duration-300">
       {text}
     </button>
   );
--- src/components/secondary-btn/SecondaryBtn.component.jsx
+++ src/components/secondary-btn/SecondaryBtn.component.jsx
@@ -1,6 +1,6 @@
 export default function SecondaryBtn({ text }) {
   return (
-    <button className="bg-transparent text-black px-16 py-4 rounded-full text-sm font-poppins border-[1px] hover:bg-secondary hover:text-white hover:border-transparent transition-all duration-300">
+    <button className="bg-transparent border text-black p-4 rounded-[0.5rem] w-44 text-sm font-poppins hover:bg-primary hover:text-white transition-all duration-300">
       {text}
     </button>
   );
