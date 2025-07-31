a1711e1638dc67e1a651a1cafe3a8bed6bc692c6
M-Taqi796 <itstaqi2919@gmail.com>
2025-07-31 18:34:40 +0500

    Added: Intro, CTA Buttons, Business card and get in touch section to header/banner

--- src/components/Banner/Banner.component.jsx
+++ src/components/Banner/Banner.component.jsx
@@ -1,11 +1,56 @@
-import BusinessCard from '../business-card/business-card.component';
+import BusinessCard from "../business-card/business-card.component";
+import PrimaryBtn from "../primary-btn/PrimaryBtn.component";
+import SecondaryBtn from "../secondary-btn/SecondaryBtn.component";
+import Arrow from "../../assets/Banner/Arrow2.svg";
+import Whatsapp from "../../assets/Banner/V-Whatsapp.svg";
+import Mail from "../../assets/Banner/V-Mail.svg";
+import Linkedin from "../../assets/Banner/V-Linkedin.svg";
+
 const Header = () => {
   return (
-    <header className="header">
-      <p>I am header</p>
+    <header className="mx-32 flex justify-between items-center my-40">
+      <div className="max-w-[39rem]">
+        <div>
+          <h1 className="font-nura text-6xl">TeraOnic</h1>
+          <h2 className="text-4xl">From Code To Impact</h2>
+          <p className="text-sm">
+            TeraOnics builds custom software tailored to your business needs
+            fast, reliable, and beautifully designed. From startups to
+            enterprises, we turn complex challenges into seamless digital
+            solutions that scale.
+          </p>
+        </div>
+
+        <div className="my-6 flex gap-2.5 flex-row">
+          <PrimaryBtn text="Free Consultation" />
+          <SecondaryBtn text="Visit us" />
+        </div>
+
+        <div className="flex gap-4 text-m">
+          <div>
+            <h3 className="font-bold">AI</h3>
+            <p>Services</p>
+          </div>
+          <div className="border-l pl-4">
+            <h3 className="font-bold">Web & App</h3>
+            <p>Development</p>
+          </div>
+          <div className="border-l pl-4">
+            <h3 className="font-bold">Ui/Ux</h3>
+            <p>Designing</p>
+          </div>
+        </div>
+      </div>
       <BusinessCard />
+      <div className="flex gap-6 flex-col items-center">
+        <p className="rotate-90 mb-6">Get in touch </p>
+        <img src={Arrow} alt="Arrow" />
+        <img src={Whatsapp} alt="Whatsapp" />
+        <img src={Mail} alt="Mail" />
+        <img src={Linkedin} alt="Linkedin" />
+      </div>
     </header>
-    );
-}
+  );
+};
 
 export default Header;
