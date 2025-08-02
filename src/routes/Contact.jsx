fcd5d93cf7450d8700261d6c8523592e17e67c89
M-Taqi796 <itstaqi2919@gmail.com>
2025-08-02 16:02:49 +0500

    added: Contact us and Get in touch Component

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,6 +1,10 @@
+import ContactUs from "../components/contact-us/Contact-us.component"
+import GetInTouch from "../components/get-in-touch/Get-in-touch.component"
 export default function Contact() {
     return(
         <>
+        <ContactUs />
+        <GetInTouch />
         </>
     )
 }
