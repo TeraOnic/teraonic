ca8134a0df7ee049a7af907cb1ffba881f71b3af
M-Taqi796 <itstaqi2919@gmail.com>
2025-10-11 21:21:09 +0500

    replaced: new services card component

--- src/components/services-card/ServicesCard.component.jsx
+++ src/components/services-card/ServicesCard.component.jsx
@@ -1,14 +1,11 @@
 import WebDevImg from "../../assets/services/4.webp";
 import Arrow from "../../assets/services/arrow.svg";
-export default function ServicesCard({ title, picture }) {
+export default function ServicesCard({ title, picture, description }) {
   return (
-    <div className="hover:shadow-[12px_22px_4px_rgba(0,0,0,0.25)] transition-all duration-300 rounded-m group font-poppins">
-      <img src={picture} alt="Service Image" className="w-96 h-96 rounded-tl-m rounded-tr-m object-cover object-center" />
-      <div className="bg-primary text-white w-96 px-6 py-7 flex justify-between rounded-bl-m rounded-br-m text-m group-hover:bg-secondary transition-all duration-300">
-        <p>{title}</p>
-        <p></p>
-        <img src={Arrow} alt="Arrow icon" />
-      </div>
+    <div className="font-poppins w-64 bg-primary rounded-2xl py-6 px-3">
+      <img src={picture} alt="" />
+      <h3 className="text-2xl text-white my-3">{title}</h3>
+      <p className="text-sm text-gray-400">{description}</p>
     </div>
   );
 }
